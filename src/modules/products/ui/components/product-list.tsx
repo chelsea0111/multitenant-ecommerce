f488c7f646cbf0132da4769abce7f5841a3015a7
chelsea0111 <xuxinyi9977@gmail.com>
2025-08-20 22:43:52 +1200

    nuqs

--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -13,7 +13,16 @@ const ProductList = ({ category }: Props) => {
   const { data } = useSuspenseQuery(
     trpc.products.getMany.queryOptions({ category })
   );
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+      {data?.docs.map((prod) => (
+        <div key={prod.id} className="border rounded-md bg-white p-4">
+          <h2 className="text-xl font-medium">{prod.name}</h2>
+          <h2>${prod.price}</h2>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
