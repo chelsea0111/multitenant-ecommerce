ce919e4902b69f567f41deddc4c84dbd69647e25
chelsea0111 <xuxinyi9977@gmail.com>
2025-08-27 00:32:03 +1200

    multi tenants

---
+++ src/collections/Tenants.ts
@@ -0,0 +1,51 @@
+import type { CollectionConfig } from "payload";
+
+export const Tenants: CollectionConfig = {
+  slug: "tenants",
+  admin: {
+    useAsTitle: "slug",
+  },
+  fields: [
+    {
+      name: "name",
+      required: true,
+      type: "text",
+      label: "Store Name",
+      admin: { description: "This is the name of the store." },
+    },
+    {
+      name: "slug",
+      required: true,
+      type: "text",
+      index: true,
+      unique: true,
+      admin: {
+        description:
+          "This is the subdomain for the store. [e.g. [slug].learnity.com]",
+      },
+    },
+    {
+      name: "image",
+      type: "upload",
+      relationTo: "media",
+    },
+    {
+      name: "stripeAccountId",
+      type: "text",
+      required: true,
+      admin: {
+        readOnly: true,
+      },
+    },
+    {
+      name: "stripeDetailsSubmitted",
+      type: "checkbox",
+      required: true,
+      admin: {
+        readOnly: true,
+        description:
+          "You cannot create products until you submitted Stripe details.",
+      },
+    },
+  ],
+};
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,8 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
+import { multiTenantPlugin } from "@payloadcms/plugin-multi-tenant";
+import type { Config } from "./payload-types";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
@@ -12,6 +14,7 @@ import { Media } from "./collections/Media";
 import { Categories } from "./collections/Categories";
 import { Products } from "./collections/Products";
 import { Tags } from "./collections/Tag";
+import { Tenants } from "./collections/Tenants";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -23,7 +26,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, Categories, Products, Tags],
+  collections: [Users, Media, Categories, Products, Tags, Tenants],
   cookiePrefix: "learnity",
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || "",
@@ -36,6 +39,14 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    multiTenantPlugin<Config>({
+      collections: {
+        products: {}, // the collection which will be tied to the tenant
+      },
+      tenantsArrayField: { includeDefaultField: false },
+      userHasAccessToAllTenants: (user) =>
+        Boolean(user?.roles?.includes("super-admin")),
+    }),
     // storage-adapter-placeholder
   ],
 });
